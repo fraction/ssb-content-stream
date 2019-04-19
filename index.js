ae2c2912461b429dd4ad65893b5ce33081ab1f34
Christian Bundy <christianbundy@fraction.io>
2019-04-19 15:42:10 -0700

    Add createContentStream method to get blobs and messages

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-const package = require('./package.json')
+const pkg = require('./package.json')
+const pull = require('pull-stream')
+const pullCat = require('pull-cat')
+const ssbRef = require('ssb-ref')
+const pullCatch = require('pull-catch')
 
-exports.init = function (ssbServer) {
-  return {}
+var MB = 1024 * 1024
+var MAX_SIZE = 5 * MB
+
+// https://github.com/ssbc/ssb-backlinks/blob/master/emit-links.js#L47-L55
+function walk (obj, fn) {
+  if (obj && typeof obj === 'object') {
+    for (var k in obj) {
+      walk(obj[k], fn)
+    }
+  } else {
+    fn(obj)
+  }
 }
 
+exports.init = (api) => (opts) => {
+  const chs = api.createHistoryStream
+
+  return pullCat([
+    pull(
+      chs(opts),
+      pull.map(msg => {
+        // get blobs referenced by message
+        // TODO: use ssb-backlinks or something to cache
+        const blobs = []
+        walk(msg, (val) => {
+          if (ssbRef.isBlob(val)) {
+            blobs.push(val)
+          }
+        })
+
+        return blobs
+      }),
+      pull.flatten(), // items of arrays of strings => items of strings
+      pull.unique(), // don't get the same blob twice
+      pull.map((hash) =>
+        pull(
+          api.blobs.get({
+            hash,
+            max: opts.max || MAX_SIZE
+          }),
+          pullCatch() // disregard blobs that don't exist or are too big
+        )
+      ),
+      pull.flatten() // convert from pull-stream source to items
+    ),
+    chs(opts)
+  ])
+}
 
 exports.manifest = {}
-exports.name = package.name
-exports.version = package.version
+exports.name = pkg.name.replace('ssb-', '')
+exports.version = pkg.version
--- test.js
+++ test.js
@@ -1,6 +1,35 @@
-var ssbServer = require('ssb-server')
+const ssbServer = require('ssb-server')
+const ssbConfig = require('ssb-config')
+const pull = require('pull-stream')
+const pullTee = require('pull-tee')
 
 ssbServer
   .use(require('ssb-server/plugins/master'))
+  .use(require('ssb-blobs'))
   .use(require('./'))
 
+const api = ssbServer(ssbConfig)
+
+const streamOpts = Object.assign(
+  {},
+  api.whoami()
+)
+
+pull(
+  api.createContentStream(streamOpts),
+  pullTee(
+    pull(
+      pull.filter(Buffer.isBuffer),
+      pull.collect(function (err, blobs) {
+        if (err) throw err
+        console.log('blobs: ' + blobs.length)
+      })
+    )
+  ),
+  pull.filter(item => !Buffer.isBuffer(item)),
+  pull.collect((err, msgs) => {
+    if (err) throw err
+    console.log('messages: ' + msgs.length)
+    api.close()
+  })
+)

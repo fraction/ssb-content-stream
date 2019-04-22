319a14cd8173d8b6288e946ffd3b2dbfe5927ac4
Christian Bundy <christianbundy@fraction.io>
2019-04-22 10:43:25 -0700

    Refactor API to avoid using plugin namespace as a function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const pull = require('pull-stream')
 const pullCat = require('pull-cat')
 const ssbRef = require('ssb-ref')
 const pullCatch = require('pull-catch')
+const pullTee = require('pull-tee')
 
 var MB = 1024 * 1024
 var MAX_SIZE = 5 * MB
@@ -18,39 +19,69 @@ function walk (obj, fn) {
   }
 }
 
-exports.init = (api) => (opts) => {
-  const chs = api.createHistoryStream
+exports.init = (ssb) => {
+  const contentStream = {
+    createSource: (opts) => pullCat([
+      pull(
+        ssb.createHistoryStream(opts),
+        pull.map(msg => {
+          // get blobs referenced by message
+          // TODO: use ssb-backlinks or something to cache
+          const blobs = []
+          walk(msg, (val) => {
+            if (ssbRef.isBlob(val)) {
+              blobs.push(val)
+            }
+          })
 
-  return pullCat([
-    pull(
-      chs(opts),
-      pull.map(msg => {
-        // get blobs referenced by message
-        // TODO: use ssb-backlinks or something to cache
-        const blobs = []
-        walk(msg, (val) => {
-          if (ssbRef.isBlob(val)) {
-            blobs.push(val)
-          }
-        })
-
-        return blobs
-      }),
-      pull.flatten(), // items of arrays of strings => items of strings
-      pull.unique(), // don't get the same blob twice
-      pull.map((hash) =>
-        pull(
-          api.blobs.get({
-            hash,
-            max: opts.max || MAX_SIZE
-          }),
-          pullCatch() // disregard blobs that don't exist or are too big
-        )
+          return blobs
+        }),
+        pull.flatten(), // items of arrays of strings => items of strings
+        pull.unique(), // don't get the same blob twice
+        pull.map((hash) =>
+          pull(
+            ssb.blobs.get({
+              hash,
+              max: opts.max || MAX_SIZE
+            }),
+            pullCatch() // disregard blobs that don't exist or are too big
+          )
+        ),
+        pull.flatten() // convert from pull-stream source to items
       ),
-      pull.flatten() // convert from pull-stream source to items
-    ),
-    chs(opts)
-  ])
+      ssb.createHistoryStream(opts)
+    ]),
+    createBlobHandler: (cb) => {
+      let count = 0
+      const errors = []
+
+      return pull(
+        pullTee(
+          pull(
+            pull.filter(Buffer.isBuffer),
+            pull.drain((blob) => {
+              pull(
+                pull.values([blob]),
+                ssb.blobs.add((err) => {
+                  if (err) errors.push(err)
+                  count += 1
+                })
+              )
+            }, (err) => {
+              if (err || errors.length) {
+                const errConcat = [err, errors]
+                return cb(errConcat, count)
+              } else {
+                cb(null, count)
+              }
+            })
+          )
+        ),
+        pull.filterNot(Buffer.isBuffer)
+      )
+    }
+  }
+  return contentStream
 }
 
 exports.manifest = {}
--- test.js
+++ test.js
@@ -1,35 +1,34 @@
 const ssbServer = require('ssb-server')
 const ssbConfig = require('ssb-config')
 const pull = require('pull-stream')
-const pullTee = require('pull-tee')
 
 ssbServer
   .use(require('ssb-server/plugins/master'))
   .use(require('ssb-blobs'))
   .use(require('./'))
 
-const api = ssbServer(ssbConfig)
+const ssb = ssbServer(ssbConfig)
 
 const streamOpts = Object.assign(
   {},
-  api.whoami()
+  ssb.whoami()
 )
 
+const count = {
+  messages: 0,
+  blobs: 0
+}
+
 pull(
-  api.createContentStream(streamOpts),
-  pullTee(
-    pull(
-      pull.filter(Buffer.isBuffer),
-      pull.collect(function (err, blobs) {
-        if (err) throw err
-        console.log('blobs: ' + blobs.length)
-      })
-    )
-  ),
-  pull.filter(item => !Buffer.isBuffer(item)),
+  ssb.contentStream.createSource(streamOpts),
+  ssb.contentStream.createBlobHandler((err, blobNum) => {
+    if (err) throw err
+    count.blobs = blobNum
+  }),
   pull.collect((err, msgs) => {
     if (err) throw err
-    console.log('messages: ' + msgs.length)
-    api.close()
+    count.messages = msgs.length
+
+    ssb.close(() => console.log(count))
   })
 )
